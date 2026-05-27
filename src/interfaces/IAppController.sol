e85aff12e4e35a8f66b44c530f3a4df6ebf8f450
Sean McGary <sean@seanmcgary.com>
2026-05-27 09:56:26 -0500

    feat: add CREATED status to AppStatus enum

--- src/interfaces/IAppController.sol
+++ src/interfaces/IAppController.sol
@@ -68,6 +68,7 @@ interface IAppController {
     /// @notice Enum for app status
     enum AppStatus {
         NONE, // App has not been created yet
+        CREATED, // App has been created but has no release and is not billing
         STARTED, // App has been started
         STOPPED, // App has been stopped but can be restarted
         TERMINATED, // App is permanently terminated
